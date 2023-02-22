ccad6dda24765484213ca56faabc2887651f7106
K-Tecza <120129028+K-Tecza@users.noreply.github.com>
2023-02-22 21:09:06 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+    float a, b, x ;
+    cout << "Podaj Pierwsza liczbe :  ";
+    cin >> a;
+    cout << "Podaj Druga liczbe :  ";
+    cin >> b;
+    x = a +b ;
+    cout << "Suma tych liczb wynosi; " << x;
+
+    return 0;
+}
